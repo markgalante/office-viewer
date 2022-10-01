f8696f4e70e0772204c14e2df0b66d6e2a85af2a
markgalante <50637189+markgalante@users.noreply.github.com>
2022-10-01 23:48:24 +0200

    chore: name change
    AddOffice to AddEditOffice

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import AddOffice from './scenes/AddOffice';
+import AddEditOffice from './scenes/AddEditOffice';
 
 function App() {
   return (
     <div id='app-container'>
-      <AddOffice />
+      <AddEditOffice />
     </div>
   );
 }
