d5038c469f6d453f9f33542b58120e814a581c21
markgalante <50637189+markgalante@users.noreply.github.com>
2022-10-02 23:10:15 +0200

    chore: create feat for managing office info state

---
+++ src/scenes/AddEditOffice/hooks/useOfficeInfo/index.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import type {
+  OfficeType,
+  OfficeColorType
+} from '../../../../hooks';
+
+export type UseOfficeInfoResult = {
+  officeInfo: OfficeType,
+  setOfficeName: (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  setOfficeAddress: (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  setEmailAddress: (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  setPhoneNumber: (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  setMaxCapacity: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  setColor: (officeColor: OfficeColorType) => void;
+  populateOfficeInfo: (officeInfo: OfficeType) => void;
+}
+
+export function useOfficeInfo(): UseOfficeInfoResult{
+  const [
+    officeInfo,
+    setOfficeInfo
+  ] = React.useState<OfficeType>({
+    address: '',
+    emailAddress: '',
+    maximumCapacity: 1,
+    name: '',
+    officeColor: '#FFBE0B',
+    phoneNumber: '',
+  });
+  const populateOfficeInfo = (officeInfo: OfficeType) => {
+    setOfficeInfo(officeInfo);
+  }
+  const setOfficeName = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const name = e.target.value;
+    setOfficeInfo({
+      ...officeInfo,
+      name,
+    });
+  };
+  const setOfficeAddress = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const address = e.target.value;
+    setOfficeInfo({
+      ...officeInfo,
+      address,
+    });
+  };
+  const setEmailAddress = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const emailAddress = e.target.value;
+    setOfficeInfo({
+      ...officeInfo,
+      emailAddress,
+    });
+  };
+  const setPhoneNumber = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const phoneNumber = e.target.value;
+    setOfficeInfo({
+      ...officeInfo,
+      phoneNumber,
+    });
+  };
+  const setMaxCapacity = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const maximumCapacity = e.target.value;
+    setOfficeInfo({
+      ...officeInfo,
+      maximumCapacity: Number(maximumCapacity),
+    });
+  };
+  const setColor = (officeColor: OfficeColorType) => {
+    setOfficeInfo({
+      ...officeInfo,
+      officeColor,
+    });
+  };
+
+  return {
+    officeInfo,
+    setColor,
+    setEmailAddress,
+    setMaxCapacity,
+    setOfficeAddress,
+    setOfficeName,
+    setPhoneNumber,
+    populateOfficeInfo,
+  }
+};
