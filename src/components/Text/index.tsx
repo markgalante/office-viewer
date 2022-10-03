c67c87ef4fd1e2b0deeb93c3f5aef19e7b230c30
markgalante <50637189+markgalante@users.noreply.github.com>
2022-10-03 21:51:30 +0200

    chore: changes to re-useable components

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,26 +1,38 @@
 import * as React from 'react';
 import './styles.css'
 
-type TextProps = {
-  type?: TextTypes;
-  center?: boolean;
-  children: React.ReactNode;
-};
-
 type TextTypes =
   | 'default'
   | 'sub-heading'
   | 'section-header'
+  | 'bold-heading'
+
+type TextAlignTypes =
+  | 'align-left'
+  | 'align-center';
+
+  type TextProps = {
+    type?: TextTypes;
+    center?: boolean;
+    className?: string;
+    children: React.ReactNode;
+    textAlign?: TextAlignTypes
+  };
 
 function Text({
   type = 'default',
   children,
+  className,
+  textAlign,
   center = false,
 }: TextProps){
   return (
     <p
       className={[
+        'standard-font-style',
         type,
+        textAlign,
+        className,
         center ? 'center' : ''
       ].join(' ')}
     >
