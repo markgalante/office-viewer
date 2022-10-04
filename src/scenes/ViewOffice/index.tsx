2f39fc95ec37514c542f6315d0f7249f2f083cf5
markgalante <50637189+markgalante@users.noreply.github.com>
2022-10-04 08:37:25 +0200

    fix: add div for company card displays

--- src/scenes/ViewOffice/index.tsx
+++ src/scenes/ViewOffice/index.tsx
@@ -2,21 +2,30 @@ import * as React from 'react';
 import {
   useParams,
 } from 'react-router-dom';
-import {CompanyCard} from '../Home';
-import {useFetchOffice, Header} from '../AddEditOffice/'
+import {
+  CompanyCard,
+} from '../Home';
+import {
+  useFetchOffice,
+  Header,
+} from '../AddEditOffice/'
+import {
+  Flex
+} from '../../components';
 
 export function ViewOffice(){
   const {uid} = useParams()
   const {
     data
   } = useFetchOffice(uid ?? '');
-  console.log('data', data)
   return (
     <>
       <Header title='Office' />
-      {
-        data ? <CompanyCard officeInfo={data}/> : null
-      }
+      <Flex alignItems='center' flexDirection='column'>
+        {
+          data ? <CompanyCard officeInfo={data}/> : null
+        }
+      </Flex>
     </>   
   )
 };
